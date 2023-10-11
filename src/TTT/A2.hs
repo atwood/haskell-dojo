422298955d230ae666fcbb2ea9eb4614a6ec7ff7
John Atwood <jw_atwood@yahoo.com>
2023-10-11 19:59:13 +0000

    thru A2, Q8

--- src/TTT/A2.hs
+++ src/TTT/A2.hs
@@ -2,6 +2,7 @@ module TTT.A2 where
 
 import Data.List (intercalate)
 import TTT.A1
+import GHC.Stack.Types (CallStack(EmptyCallStack))
 
 -- Q#01
 
@@ -13,7 +14,7 @@ promptPlayer p = do
                    ,show p
                    , "'s turn: enter a row and column position (ex. A1)"
                    ] 
-    return (getLine)
+    return getLine
 -- Q#02
 
 -- _RANGE_ = undefined
@@ -21,33 +22,51 @@ _RANGE_ = [0..(-1 + _SIZE_)]
 
 -- Q#03
 
-isDigit = undefined
+-- isDigit = undefined
+isDigit c = elem c ['0'..'9']
 
-readDigit = undefined
-
+-- readDigit = undefined
+readDigit c | isDigit c = read $ c:""
+readDigit c | True = -1
 -- Q#04
 
-_EMPTY_ROW_ = undefined
+-- _EMPTY_ROW_ = undefined
+_EMPTY_ROW_ = replicate (fromInteger _SIZE_) Empty
 
-_EMPTY_BOARD_ = undefined
+-- _EMPTY_BOARD_ = undefined
+_EMPTY_BOARD_ = replicate (fromInteger _SIZE_) _EMPTY_ROW_
 
 -- Q#05
 
-isTied = undefined
+-- isTied = undefined
+isTied b = not.any (==Empty) $ foldl1 (++) b
 
-_TIED_BOARD_ = undefined
+-- _TIED_BOARD_ = undefined
+_TIED_BOARD_ :: Board
+_TIED_BOARD_ = [
+        [X, O, O]
+      , [O, X, X]
+      , [O, X, O]
+      ]
 
 -- Q#06
 
-indexRowStrings = undefined
-
+-- indexRowStrings = undefined
+indexRowStrings lst = zip ['A'..] lst
 -- Q#07
 
-formatLine = undefined
+-- formatLine = undefined
+formatLine lst = concat [_SEP_
+                        ,intercalate _SEP_ lst
+                        ,_SEP_
+                        ]
 
 -- Q#08
 
-isMoveInBounds = undefined
+-- isMoveInBounds = undefined
+isMoveInBounds (r,c) = isRowInBounds r && (isColInBounds c)
+isRowInBounds n = elem n [0..(-1 + _SIZE_)]
+isColInBounds = isRowInBounds
 
 -- Q#09
 
