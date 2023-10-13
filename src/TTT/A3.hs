fcaa9444f5a52767fc71c5347a8b13aa99293868
John Atwood <jw_atwood@yahoo.com>
2023-10-13 20:18:18 +0000

    thru A3, Q5

--- src/TTT/A3.hs
+++ src/TTT/A3.hs
@@ -3,6 +3,7 @@ module TTT.A3 where
 import Data.List (transpose)
 import TTT.A1
 import TTT.A2
+import GHC.IO.Handle (hClose)
 
 -- Q#01
 
@@ -12,27 +13,44 @@ import TTT.A2
 showInts [] = []
 showInts (h:t) = show h : (showInts t)
 
-t = 0 
-
-_HEADER_ = undefined
+{- t = 0 
+r [] = []
+r [x] =[x]
+r (h:t) = (r t)++[h]
+-}
+-- _HEADER_ = undefined
+_HEADER_ = ' ' : formatLine (showInts _RANGE_)
 
 -- Q#02
 
-showSquares = undefined
-
+-- showSquares = undefined
+--showSquares [] = []
+--showSquares lst = ' ':(formatLine lst)
+showSquares lst = map show lst
 -- Q#03
 
-formatRows = undefined
+--formatRows = undefined
+--formatRows brd = map showSquares brd
+formatRows brd = map formatLine (map showSquares brd)
 
 -- Q#04
 
-isColEmpty = undefined
+{- JWA: 2023Oct13 This assignment seems malformed: isColEmpty doesn't need to be recursive, 
+        but a function isRowEmpty might well be
+-}
+--isColEmpty = undefined
+isColEmpty row idx = Empty == row !! idx
+
+isRowEmpty (c:[]) = (c==Empty)
+isRowEmpty (c:tail) = (c==Empty) && (isRowEmpty tail)
 
 -- Q#05
 
-dropFirstCol = undefined
+--dropFirstCol = undefined
+dropFirstCol board = map (drop 1) board
 
-dropLastCol = undefined
+--dropLastCol = undefined
+dropLastCol board = map (take (fromIntegral (_SIZE_ - 1))) board
 
 -- Q#06
 
